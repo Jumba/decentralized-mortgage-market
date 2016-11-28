fc6ea81ecf6d328c35715cb860bd5204595363b3
seuushi <seumanto@gmail.com>
2016-11-28 15:54:41 +0100

    Added empty functions from the investor's perspective

--- market/api/api.py
+++ market/api/api.py
@@ -20,3 +20,27 @@ class MarketAPI(object):
     def create_user(self):
         # bla bla
         self.post('user', ('example', 'example2'))
+
+    def login_user(self):
+        """ get the user_key """
+        pass
+
+    def modify_profile(self):
+        """ post the data in profile """
+        pass
+
+    def place_loan_offer(self):
+        """ post the data needed for placing a loan offer """
+        pass
+
+    def resell_investment(self):
+        """ post the data needed to resell the investment """
+        pass
+
+    def load_investments(self):
+        """ get the data from current and pending investments """
+        pass
+
+    def load_open_market(self):
+        """ get the 'to be displayed on the open market' data  """
+        pass
