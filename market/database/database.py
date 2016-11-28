24413dd43076ca137f0b657915094fb17744299e
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-28 19:44:08 +0100

    Added database tests

--- market/database/database.py
+++ market/database/database.py
@@ -1,12 +1,63 @@
+import uuid
+
+from market.database.backends import Backend
+from market.models import DatabaseModel
+
+
 class Database(object):
     def get(self, type, id):
-        return NotImplementedError
+        raise NotImplementedError
 
     def post(self, type, obj):
-        return NotImplementedError
+        raise NotImplementedError
 
     def put(self, type, id, obj):
-        return NotImplementedError
+        raise NotImplementedError
 
-    def delete(self, id):
-        return NotImplementedError
+    def delete(self, type, id):
+        raise NotImplementedError
+
+
+class MockDatabase(Database):
+    def __init__(self, backend):
+        assert isinstance(backend, Backend)
+
+        self._backend = backend
+
+    def get(self, type, id):
+        try:
+            return DatabaseModel.decode(self._backend.get(type, id))
+        except IndexError:
+            return None
+
+    def post(self, type, obj):
+        assert isinstance(obj, DatabaseModel)
+        try:
+            id = uuid.uuid4()
+            while not self.backend.id_available(id):
+                id = uuid.uuid4()
+
+            obj.save(id)
+            self.backend.post(type, id, obj.encode())
+            return id
+        except IndexError:
+            return False
+
+    def put(self, type, id, obj):
+        assert isinstance(obj, DatabaseModel)
+
+        assert obj.id
+        assert id == obj.id
+
+        try:
+            return self.backend.put(type, id, obj.encode())
+        except IndexError:
+            return False
+
+    def delete(self, obj):
+        assert isinstance(obj, DatabaseModel)
+        raise NotImplementedError
+
+    @property
+    def backend(self):
+        return self._backend
--- market/models/__init__.py
+++ market/models/__init__.py
@@ -2,12 +2,21 @@ import pickle
 
 
 class DatabaseModel(object):
+    _type = 'database_model'
+
+    def __init__(self, id=None):
+        self._id = id
+
     def save(self, id):
         self._id = id
 
     @property
     def id(self):
         return self._id
+
+    @property
+    def type(self):
+        return self._type
 
     def encode(self, encoding='base64'):
         """
@@ -22,3 +31,6 @@ class DatabaseModel(object):
     def decode(data, encoding='base64'):
         pickled = data.decode(encoding)
         return pickle.loads(pickled)
+
+    def __eq__(self, other):
+        return self.id == other.id
