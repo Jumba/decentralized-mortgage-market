e1a7d9bc0020b67894e5762966d774449dd65de9
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-29 12:57:03 +0100

    MarketAPI tests

---
+++ test/market/test_api.py
@@ -0,0 +1,34 @@
+import unittest
+
+from market.api.api import MarketAPI
+from market.database.backends import MemoryBackend
+from market.database.database import MockDatabase
+from market.dispersy.crypto import ECCrypto
+from market.models.user import User
+
+
+class APITestSuite(unittest.TestCase):
+    def setUp(self):
+        self.database = MockDatabase(MemoryBackend())
+        self.api = MarketAPI(self.database)
+        self.ec = ECCrypto()
+
+    def test_create_user(self):
+        user, pub, priv = self.api.create_user()
+
+        # Check if the user object is returned
+        self.assertIsInstance(user, User)
+
+        # Check if the public and private keys are valid
+        self.assertTrue(self.ec.is_valid_public_bin(pub.decode("HEX")))
+        self.assertTrue(self.ec.is_valid_private_bin(priv.decode("HEX")))
+
+    def test_login_user(self):
+        # Create a user
+        user, pub, priv = self.api.create_user()
+
+        # Login the user using his private key
+        user_login = self.api.login_user(priv)
+
+        # Confirm is the user returned is equal to the user created.
+        self.assertEqual(user, user_login)
