f4e71fbef930fa2283a71e54cb75dd933a451ede
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-29 15:30:50 +0100

    Example of a setter

--- market/models/user.py
+++ market/models/user.py
@@ -4,9 +4,10 @@ from market.models import DatabaseModel
 class User(DatabaseModel):
     _type = 'users'
 
-    def __init__(self, public_key, time_added, profile_id, loan_request_id, mortgage_ids, investment_ids):
+    def __init__(self, public_key, time_added, role_id=None, profile_id=None, loan_request_id=None, mortgage_ids=[], investment_ids=[]):
         self._public_key = public_key
         self._time_added = time_added
+        self._role_id = role_id
         self._profile_id = profile_id
         self._loan_request_id = loan_request_id
         self._mortgage_ids = mortgage_ids
@@ -19,9 +20,6 @@ class User(DatabaseModel):
     @property
     def time_added(self):
         return self._time_added
-
-    def generate_id(self):
-        return self.user_key
 
     @property
     def profile_id(self):
@@ -38,3 +36,18 @@ class User(DatabaseModel):
     @property
     def investment_ids(self):
         return self._investment_ids
+
+    @property
+    def role_id(self):
+        return self._role_id
+
+    def generate_id(self):
+        return self.user_key
+
+    @profile_id.setter
+    def profile_id(self, value):
+        self._profile_id = value
+
+    @role_id.setter
+    def role_id(self, value):
+        self._role_id = value
