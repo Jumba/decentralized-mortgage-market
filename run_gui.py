92471b3d4c5b4fc65726884730d618d17c6b0f5e
arthur <a.hovanesyan@gmail.com>
2016-11-30 12:16:39 +0100

    Login: changed ui object names

--- run_gui.py
+++ run_gui.py
@@ -1,23 +1,20 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-
 import sys
 import os
 
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from market.views import login
 
 
-class ExampleApp(QMainWindow, login.Ui_Form): #QtWidgets.QMainWindow and login.Ui_Form are the superclasses
+class LoginController(QMainWindow, login.Ui_Form):
     def __init__(self, parent=None):
-        super(ExampleApp, self).__init__(parent)
+        super(LoginController, self).__init__(parent)
         self.setupUi(self)
         self.toolButton.clicked.connect(self.browse)
-        #self.toolButton.clicked.connect(self.browse) kan ook
         self.pushButton.clicked.connect(self.login)
 
     def browse(self):
         filename, _ = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
-
         fh = ''
 
         if QFile.exists(filename):
@@ -30,19 +27,16 @@ class ExampleApp(QMainWindow, login.Ui_Form): #QtWidgets.QMainWindow and login.U
         codec = QTextCodec.codecForUtfText(data)
         unistr = codec.toUnicode(data)
 
-        tmp = ('Notepad: %s' % filename)
-        self.setWindowTitle(tmp)
         self.lineEdit.setText(unistr)
 
     def login(self):
+        #if remember me == true
         print self.lineEdit.text()
-
 
 
 def main():
     app = QApplication(sys.argv)
-    form = ExampleApp()
-    #form.toolButton.clicked.connect(form.browse())
+    form = LoginController()
     form.show()
     app.exec_()
 
