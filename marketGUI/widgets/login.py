5d62d460ddd9fcd84df81acbe9a2500e12f22907
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-30 12:16:39 +0100

    Attempting to copy the Tribler GUI hierarchy, but failing. Code not working

---
+++ marketGUI/widgets/login.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login.ui'
+#
+# Created by: PyQt5 UI code generator 5.4.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from PyQt5.QtWidgets import QWidget
+
+
+class LoginPage(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+    def initialize(self, Form=None):
+        Form.setObjectName("Form")
+        Form.resize(650, 273)
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(0, 0, 651, 51))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(30, 50, 101, 16))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(30, 70, 481, 23))
+        self.lineEdit.setObjectName("lineEdit")
+        self.toolButton = QtWidgets.QToolButton(Form)
+        self.toolButton.setGeometry(QtCore.QRect(530, 70, 71, 22))
+        self.toolButton.setObjectName("toolButton")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(430, 100, 80, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.checkBox = QtWidgets.QCheckBox(Form)
+        self.checkBox.setGeometry(QtCore.QRect(304, 100, 111, 21))
+        self.checkBox.setObjectName("checkBox")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(30, 140, 651, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_3.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.label_3.setObjectName("label_3")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(30, 180, 80, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "Login"))
+        self.label_2.setText(_translate("Form", "Private key"))
+        self.toolButton.setText(_translate("Form", "Browse"))
+        self.pushButton.setText(_translate("Form", "Login"))
+        self.checkBox.setText(_translate("Form", "Remember Me"))
+        self.label_3.setText(_translate("Form", "Don\'t have a private key?"))
+        self.pushButton_2.setText(_translate("Form", "Generate "))
+
