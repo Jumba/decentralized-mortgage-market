e201b1acd1e25974a279fcf2d3d2964dc5796180
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-30 14:03:50 +0100

    Added support for a persistent database

--- market/database/backends.py
+++ market/database/backends.py
@@ -1,5 +1,9 @@
-class Backend(object):
+from os import path
 
+from market.dispersy.database import Database
+
+
+class Backend(object):
     def get(self, type, id):
         raise NotImplementedError
 
@@ -16,6 +20,9 @@ class Backend(object):
         raise NotImplementedError
 
     def exists(self, type, id):
+        raise NotImplementedError
+
+    def clear(self):
         raise NotImplementedError
 
 
@@ -59,3 +66,90 @@ class MemoryBackend(Backend):
     def clear(self):
         self._data = {}
         self._id = {}
+
+
+class PersistentBackend(Database, Backend):
+    # Path to the database location + dispersy._workingdirectory
+    DATABASE_PATH = u"market.db"
+    # Version to keep track if the db schema needs to be updated.
+    LATEST_DB_VERSION = 1
+    # Schema for the MultiChain DB.
+    schema = u"""
+    CREATE TABLE IF NOT EXISTS market(
+     id		                    TEXT NOT NULL,
+     type_name		            TEXT NOT NULL,
+     value                      TEXT NOT NULL,
+
+     insert_time                TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+     );
+
+    CREATE TABLE option(key TEXT PRIMARY KEY, value BLOB);
+    INSERT INTO option(key, value) VALUES('database_version', '""" + str(LATEST_DB_VERSION) + u"""');
+    """
+
+    def __init__(self, working_directory):
+        super(PersistentBackend, self).__init__(path.join(working_directory, self.DATABASE_PATH))
+
+    def open(self, initial_statements=True, prepare_visioning=True):
+        return super(PersistentBackend, self).open(initial_statements, prepare_visioning)
+
+    def close(self, commit=True):
+        return super(PersistentBackend, self).close(commit)
+
+    def check_database(self, database_version):
+        assert isinstance(database_version, unicode)
+        assert database_version.isdigit()
+        assert int(database_version) >= 0
+        database_version = int(database_version)
+
+        if database_version < self.LATEST_DB_VERSION:
+            # Remove all previous data, since we have only been testing so far, and previous blocks might not be
+            # reliable. In the future, we should implement an actual upgrade procedure
+            self.executescript(self.schema)
+            self.commit()
+
+        return self.LATEST_DB_VERSION
+
+    def get(self, type, id):
+        db_query = u"SELECT value FROM `market` WHERE type_name = ? AND id = ?"
+        db_result = self.execute(db_query, (unicode(type), unicode(id))).fetchall()
+
+        if len(db_result) != 1:
+            raise IndexError
+
+        return db_result[0][0]
+
+    def post(self, type, id, obj):
+        if not self.id_available(id):
+            raise IndexError("Index already in use")
+
+        db_query = u"INSERT INTO `market` (id, type_name, value) VALUES (?, ?, ?)"
+        self.execute(db_query, (unicode(id), unicode(type), unicode(obj)))
+        self.commit()
+
+    def put(self, type, id, obj):
+        if self.exists(type, id):
+            db_query = u"UPDATE `market` SET value = ? WHERE id = ? AND type_name = ?"
+            self.execute(db_query, (unicode(obj), unicode(id), unicode(type)))
+            self.commit()
+            return True
+        else:
+            return False
+
+    def delete(self, id):
+        db_query = u"DELETE FROM `market` WHERE id = ?"
+        self.execute(db_query, (unicode(id),))
+        self.commit()
+
+    def id_available(self, id):
+        db_query = u"SELECT COUNT(*) FROM `market` WHERE id = ?"
+        db_result = self.execute(db_query, (unicode(id),)).fetchall()
+        return db_result[0][0] == 0
+
+    def exists(self, type, id):
+        db_query = u"SELECT COUNT(*) FROM `market` WHERE id = ? AND type_name = ?"
+        db_result = self.execute(db_query, (unicode(id), unicode(type))).fetchall()
+        return db_result[0][0] == 1
+
+    def clear(self):
+        self.execute(u"DELETE FROM market")
--- test/market/test_backend.py
+++ test/market/test_backend.py
@@ -1,6 +1,7 @@
 import unittest
 
-from market.database.backends import Backend, MemoryBackend
+from market.database.backends import Backend, MemoryBackend, PersistentBackend
+from market.models import DatabaseModel
 
 
 class BackendTestSuite(unittest.TestCase):
@@ -97,6 +98,72 @@ class MemoryBackendTestSuite(unittest.TestCase):
         self.backend.clear()
         with self.assertRaises(NotImplementedError):
             self.backend.delete(self.block1.id)
+
+
+class PersistentBackendTestSuite(unittest.TestCase):
+    def setUp(self):
+        self.backend = PersistentBackend('.')
+        self.backend.open()
+
+        # Create some data
+        self.block1 = DatabaseModel('1')
+        self.block2 = DatabaseModel('2')
+        self.block3 = DatabaseModel('3')
+
+    def tearDown(self):
+        self.backend.close()
+
+    def test_clear(self):
+        self.backend.clear()
+        self.backend.post('test', self.block1.id, self.block1)
+        self.backend.clear()
+        with self.assertRaises(IndexError):
+            self.backend.get('test', self.block1.id)
+
+    def test_post(self):
+        self.backend.clear()
+        self.backend.post('test', self.block1.id, self.block1.encode())
+        self.assertEqual(self.block1, DatabaseModel.decode(self.backend.get('test', self.block1.id)))
+
+    def test_get(self):
+        self.backend.clear()
+        self.backend.post('test', self.block1.id,  self.block1.encode())
+        self.backend.post('test', self.block2.id, self.block2.encode())
+        self.backend.post('test', self.block3.id, self.block3.encode())
+
+        with self.assertRaises(IndexError):
+            self.backend.post('test2', self.block1.id, self.block1.encode())
+
+        self.assertEqual(DatabaseModel.decode(self.backend.get('test', '1')), self.block1)
+        self.assertEqual(DatabaseModel.decode(self.backend.get('test', self.block2.id)), self.block2)
+        self.assertNotEqual(DatabaseModel.decode(self.backend.get('test', '1')), DatabaseModel.decode(self.backend.get('test', '2')))
+
+        with self.assertRaises(IndexError):
+            self.assertEqual(DatabaseModel.decode(self.backend.get('test', '1')), DatabaseModel.decode(self.backend.get('test2', '1')))
+
+    def test_get_error(self):
+        self.backend.clear()
+        with self.assertRaises(IndexError):
+            self.backend.get('test', 1)
+
+    def test_put_fail(self):
+        self.backend.clear()
+        self.assertFalse(self.backend.put('test', '1', self.block1))
+
+    def test_put_success(self):
+        self.backend.clear()
+        self.backend.post('test', self.block1.id, self.block1.encode())
+        self.assertEqual(DatabaseModel.decode(self.backend.get('test', self.block1.id)), self.block1)
+
+        self.assertTrue(self.backend.put('test', self.block1.id, self.block2.encode()))
+        self.assertEqual(DatabaseModel.decode(self.backend.get('test', self.block1.id)), self.block2)
+
+    def test_delete(self):
+        self.backend.clear()
+        self.backend.post('test', self.block1.id, self.block1)
+        self.backend.delete(self.block1.id)
+
+        self.assertFalse(self.backend.exists('test', self.block1.id))
 
 
 if __name__ == '__main__':
