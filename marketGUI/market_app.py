cc21a4b091fe93b004b085b9ed63abc51235ddaa
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-30 15:03:49 +0100

    Implemented the option to create a key and login using an existing key.

--- marketGUI/market_app.py
+++ marketGUI/market_app.py
@@ -1,6 +1,10 @@
 
 from PyQt5.QtWidgets import QApplication
 
+from market.api.api import MarketAPI
+from market.database.backends import PersistentBackend
+from market.database.database import MockDatabase
+
 
 class MarketApplication(QApplication):
     """
@@ -8,5 +12,9 @@ class MarketApplication(QApplication):
     """
     def __init__(self, *argv):
         QApplication.__init__(self, *argv)
+        self._api = MarketAPI(MockDatabase(PersistentBackend('.')))
 
+    @property
+    def api(self):
+        return self._api
 
