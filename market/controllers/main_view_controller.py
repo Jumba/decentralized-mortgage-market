e7b21d0c8a096f6585cb5767b4c11d1714e45b7d
arthur <a.hovanesyan@gmail.com>
2016-12-05 03:29:41 +0100

    Pending Loan Request 1 screen view and controls

--- market/controllers/main_view_controller.py
+++ market/controllers/main_view_controller.py
@@ -6,8 +6,6 @@ from PyQt5.QtWidgets import *
 
 from market.views import main_view
 from marketGUI.market_app import MarketApplication
-# from place_loan_request_controller import PlaceLoanRequestController
-import place_loan_request_controller
 
 
 class MainWindowController(QStackedWidget, main_view.Ui_StackedWidget):
@@ -15,61 +13,84 @@ class MainWindowController(QStackedWidget, main_view.Ui_StackedWidget):
         super(MainWindowController, self).__init__(parent)
         self.mainwindow = self #to make moving to another class easier
         self.app = app
+        self.banks_payload = {}
         self.setupUi(self)
         self.setCurrentIndex(2)
-        self.setNavigation()
+        self.set_navigation()
         self.bplr_submit_button.clicked.connect(self.bplr_submit_loan_request)
+        self.fiplr1_loan_requests_table.doubleClicked.connect(self.view_loan_request)
+
+
+    def view_loan_request(self):
+         print self.fiplr1_loan_requests_table.selectedIndexes()[0].row() # index of the row
+
+
+    def update_table_row(self, row, payload):
+        print self.fiplr1_loan_requests_table.item(row, 0).text()
+        print payload
+        # self.fiplr1_loan_requests_table.item(row, 0).setText(payload['postal_code'])#+' , '+payload['house_number'])
+        self.fiplr1_loan_requests_table.item(row, 0).setText(payload['postal_code']+' , '+payload['house_number'])
+        self.fiplr1_loan_requests_table.item(row, 1).setText(str(payload['mortgage_type']))
+        self.fiplr1_loan_requests_table.item(row, 2).setText(payload['amount_wanted'])
+        self.fiplr1_loan_requests_table.item(row, 3).setText(payload['price'])
+
 
     def bplr_submit_loan_request(self):
         fields = {'postal_code': self.mainwindow.bplr_postcode_lineedit,
                   'house_number': self.mainwindow.bplr_housenumber_lineedit,
                   'price': self.mainwindow.bplr_house_price_lineedit,
-                  'description': self.mainwindow.bplr_description_textedit,
                   'amount_wanted': self.mainwindow.bplr_amount_wanted_lineedit}
         banks = [self.mainwindow.bplr_bank1_checkbox, self.mainwindow.bplr_bank2_checkbox,
                  self.mainwindow.bplr_bank3_checkbox,
                  self.mainwindow.bplr_bank4_checkbox]
         banks_ids = ['1', '2', '3', '4']
-        banks_payload = []
+        checked_banks = []
+
+        # Get all the inputs
+        for key in fields:
+            self.banks_payload[key] = fields[key].text()
 
         #what banks were chosen
         pointer = 0
         for obj in banks:
             if obj.checkState():
-                banks_payload += banks_ids[pointer]
+                checked_banks += banks_ids[pointer]
             pointer += 1
-        fields['banks'] = banks_payload
+        self.banks_payload['banks'] = checked_banks
 
         #check the chosen mortgage type
         if self.mainwindow.bplr_linear_radiobutton.isChecked():
-            fields['mortgage_type'] = 0
+            self.banks_payload['mortgage_type'] = 0
         else:
-            fields['mortgage_type'] = 1
-        self.payload = fields
+            self.banks_payload['mortgage_type'] = 1
+
+        self.banks_payload['description'] = self.mainwindow.bplr_description_textedit.toPlainText(),
+
         self.setCurrentIndex(4)
+        self.update_table_row(0, self.banks_payload)
 
-    def setNavigation(self):
-        self.next_1.clicked.connect(self.nextScreen)
-        self.next_2.clicked.connect(self.nextScreen)
-        self.next_3.clicked.connect(self.nextScreen)
-        self.next_4.clicked.connect(self.nextScreen)
-        self.next_5.clicked.connect(self.nextScreen)
-        self.next_6.clicked.connect(self.nextScreen)
-        self.next_7.clicked.connect(self.nextScreen)
-        self.next_8.clicked.connect(self.nextScreen)
-        self.prev_1.clicked.connect(self.prevScreen)
-        self.prev_2.clicked.connect(self.prevScreen)
-        self.prev_3.clicked.connect(self.prevScreen)
-        self.prev_4.clicked.connect(self.prevScreen)
-        self.prev_5.clicked.connect(self.prevScreen)
-        self.prev_6.clicked.connect(self.prevScreen)
-        self.prev_7.clicked.connect(self.prevScreen)
-        self.prev_8.clicked.connect(self.prevScreen)
+    def set_navigation(self):
+        self.next_1.clicked.connect(self.next_screen)
+        self.next_2.clicked.connect(self.next_screen)
+        self.next_3.clicked.connect(self.next_screen)
+        self.next_4.clicked.connect(self.next_screen)
+        self.next_5.clicked.connect(self.next_screen)
+        self.next_6.clicked.connect(self.next_screen)
+        self.next_7.clicked.connect(self.next_screen)
+        self.next_8.clicked.connect(self.next_screen)
+        self.prev_1.clicked.connect(self.prev_screen)
+        self.prev_2.clicked.connect(self.prev_screen)
+        self.prev_3.clicked.connect(self.prev_screen)
+        self.prev_4.clicked.connect(self.prev_screen)
+        self.prev_5.clicked.connect(self.prev_screen)
+        self.prev_6.clicked.connect(self.prev_screen)
+        self.prev_7.clicked.connect(self.prev_screen)
+        self.prev_8.clicked.connect(self.prev_screen)
 
-    def nextScreen(self):
+    def next_screen(self):
         self.setCurrentIndex((self.currentIndex() + 1) % 8)
 
-    def prevScreen(self):
+    def prev_screen(self):
         self.setCurrentIndex((self.currentIndex() - 1) % 8)
 
 
