52fe9c1fc4acf222a5c3a7a810ebec6f9755e2ac
Katia <katia.asmoredjo@gmail.com>
2016-12-14 14:34:29 +0100

    Set up message queue

---
+++ market/community/queue.py
@@ -0,0 +1,11 @@
+class MessageQueue():
+    def __init__(self):
+        self._queue = []
+
+    # TODO Figure out what information we should save in the queue
+    def add_message(self, message_name, fields, models, receivers):
+        pass
+
+    # TODO Figure out how to tell when a message is sent
+    def send_message(self, message):
+        pass
