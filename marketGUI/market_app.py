eb43bc769249fc5c2ac3e0f65b24975d9d593f47
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-12-15 18:34:01 +0100

    Added the application for ING

--- marketGUI/market_app.py
+++ marketGUI/market_app.py
@@ -1,13 +1,10 @@
 import logging
 
-import time
-
 from market.models.user import User
 from scenarios.scenario import Scenario
 
 logging.basicConfig(level=logging.WARNING, filename="market.log", filemode="a+",
                     format="%(asctime)-15s %(levelname)-8s %(message)s")
-
 
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
@@ -23,13 +20,13 @@ from market.dispersy.endpoint import StandaloneEndpoint
 
 
 class MarketApplication(QApplication):
-
     port = 1236
     database_prefix = ''
 
     """
     This class represents the main Market application.
     """
+
     def __init__(self, *argv):
         QApplication.__init__(self, *argv)
         self.initialize_api()
@@ -51,7 +48,6 @@ class MarketApplication(QApplication):
     def initialize_api(self):
         self._api = MarketAPI(MockDatabase(PersistentBackend('.', u'sqlite/market.db')))
 
-
     def identify(self):
         """
         Identify the user to the system.
@@ -68,8 +64,6 @@ class MarketApplication(QApplication):
             self.private_key = priv
             print "Using a new user"
 
-
-
     def start_dispersy(self):
         self.dispersy = Dispersy(StandaloneEndpoint(self.port, '0.0.0.0'), unicode('.'), u'dispersy-%s.db' % self.database_prefix)
         self.dispersy.statistics.enable_debug_statistics(True)
@@ -82,8 +76,11 @@ class MarketApplication(QApplication):
         self.community.user = self.user
         self.api.community = self.community
 
-        # Run the scenario every 5 seconds
-        LoopingCall(lambda: self._scenario()).start(3.0)
+        # Run the scenario every 3 seconds
+        LoopingCall(self._scenario).start(3.0)
+
+        # Send messages from the queue every 3 seconds
+        LoopingCall(self.api.queue.send_messages).start(3.0)
 
     def _scenario(self):
         pass
@@ -93,15 +90,12 @@ class MarketApplication(QApplication):
         return self._api
 
 
-
 class MarketApplicationABN(MarketApplication):
-
     database_prefix = 'ABN'
     port = 1237
 
     def initialize_api(self):
         self._api = MarketAPI(MockDatabase(PersistentBackend('.', u'sqlite/%s-market.db' % self.database_prefix)))
-
 
     def identify(self):
         self.private_key = Global.BANKS_PRIV[self.database_prefix]
@@ -118,7 +112,35 @@ class MarketApplicationABN(MarketApplication):
             self.api.db.post(self.user.type, self.user)
 
         if self.user and self.user.user_key == Global.BANKS[self.database_prefix]:
-            print "Identified as ", self.database_prefix, self.user.user_key
+            print "Identified as ", self.database_prefix
+        else:
+            print "failed to identify as ", self.database_prefix
+            print "User is ", self.user
+
+
+class MarketApplicationING(MarketApplication):
+    database_prefix = 'ING'
+    port = 1239
+
+    def initialize_api(self):
+        self._api = MarketAPI(MockDatabase(PersistentBackend('.', u'sqlite/%s-market.db' % self.database_prefix)))
+
+    def identify(self):
+        self.private_key = Global.BANKS_PRIV[self.database_prefix]
+        try:
+            print "Attempting login"
+            self.user = self.api.login_user(self.private_key.encode("HEX"))
+            if not self.user:
+                raise IndexError
+        except IndexError:
+            print "Creating new user"
+            self.user = User(public_key=Global.BANKS[self.database_prefix], time_added=0)
+            self.user.role_id = 3
+            print self.user, " has been created."
+            self.api.db.post(self.user.type, self.user)
+
+        if self.user and self.user.user_key == Global.BANKS[self.database_prefix]:
+            print "Identified as ", self.database_prefix
         else:
             print "failed to identify as ", self.database_prefix
             print "User is ", self.user
