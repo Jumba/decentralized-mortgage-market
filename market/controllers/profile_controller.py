f349a8091aebd92e890decaedc887b377ceb0b69
Katia <katia.asmoredjo@gmail.com>
2016-12-15 16:04:16 +0100

    Profile page is now empty when user doesn't have a role yet

--- market/controllers/profile_controller.py
+++ market/controllers/profile_controller.py
@@ -18,31 +18,29 @@ class ProfileController:
         # self.user = self.mainwindow.app.user
         self.mainwindow.profile_save_pushbutton.clicked.connect(self.save_form)
 
-
     def setup_view(self):
-        self.current_profile = self.mainwindow.api.load_profile(self.mainwindow.app.user)
-        # print 'Profile: Current profile: ',
-        # print self.current_profile
-        if self.current_profile:
-            assert isinstance(self.current_profile, Profile)
-            self.mainwindow.profile_firstname_lineedit.setText(self.current_profile.first_name)
-            self.mainwindow.profile_lastname_lineedit.setText(self.current_profile.last_name)
-            self.mainwindow.profile_email_lineedit.setText(self.current_profile.email)
-            self.mainwindow.profile_iban_lineedit.setText(self.current_profile.iban)
-            self.mainwindow.profile_phonenumber_lineedit.setText(self.current_profile.phone_number)
+        if self.mainwindow.app.user.role_id:
+            self.current_profile = self.mainwindow.api.load_profile(self.mainwindow.app.user)
+            # print 'Profile: Current profile: ',
+            # print self.current_profile
+            if self.current_profile:
+                assert isinstance(self.current_profile, Profile)
+                self.mainwindow.profile_firstname_lineedit.setText(self.current_profile.first_name)
+                self.mainwindow.profile_lastname_lineedit.setText(self.current_profile.last_name)
+                self.mainwindow.profile_email_lineedit.setText(self.current_profile.email)
+                self.mainwindow.profile_iban_lineedit.setText(self.current_profile.iban)
+                self.mainwindow.profile_phonenumber_lineedit.setText(self.current_profile.phone_number)
 
-            if self.mainwindow.app.user.role_id == 1:
-                self.mainwindow.profile_borrower_radiobutton.setChecked(True)
-                self.mainwindow.profile_postcode_lineedit.setText(self.current_profile.current_postal_code)
-                self.mainwindow.profile_housenumber_lineedit.setText(self.current_profile.current_house_number)
-                # missing 'documents_list': self.documentsTable
+                if self.mainwindow.app.user.role_id == 1:
+                    self.mainwindow.profile_borrower_radiobutton.setChecked(True)
+                    self.mainwindow.profile_postcode_lineedit.setText(self.current_profile.current_postal_code)
+                    self.mainwindow.profile_housenumber_lineedit.setText(self.current_profile.current_house_number)
+                    # missing 'documents_list': self.documentsTable
+                else:
+                    self.mainwindow.profile_investor_radiobutton.setChecked(True)
             else:
-                self.mainwindow.profile_investor_radiobutton.setChecked(True)
-        else:
-            print 'Profile: Current profile is empty'
-            print self.current_profile
-
-
+                print 'Profile: Current profile is empty'
+                print self.current_profile
 
     def show_current_profile(self):
         # if self.current_profile:
