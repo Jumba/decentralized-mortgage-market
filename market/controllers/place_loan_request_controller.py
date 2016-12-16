438f1cb520044f571e1c7d475a3e87960d99599c
Katia <katia.asmoredjo@gmail.com>
2016-12-16 16:55:02 +0100

    Place loan request screen now opens a dialog when not all of the required information has been entered

--- market/controllers/place_loan_request_controller.py
+++ market/controllers/place_loan_request_controller.py
@@ -1,11 +1,9 @@
 from PyQt5.QtWidgets import *
-from market.views.main_view import Ui_MainWindow
 
 
 class PlaceLoanRequestController:
     def __init__(self, mainwindow):
-        # self.mainwindow = Ui_MainWindow  # Comment before running
-        self.mainwindow = mainwindow  # Uncomment before running
+        self.mainwindow = mainwindow
         self.payload = {}
         self.mainwindow.bplr_submit_button.clicked.connect(self.submit_loan_request)
 
@@ -13,36 +11,40 @@ class PlaceLoanRequestController:
         pass
 
     def submit_loan_request(self):
-        self.payload = {'postal_code': str(self.mainwindow.bplr_postcode_lineedit.text()),
-                        'house_number': str(self.mainwindow.bplr_housenumber_lineedit.text()),
-                        'price': int(self.mainwindow.bplr_house_price_lineedit.text()),
-                        'amount_wanted': int(self.mainwindow.bplr_amount_wanted_lineedit.text()),
-                        'description': unicode(self.mainwindow.bplr_description_textedit.toPlainText()),
-                        # TODO the following two fields are subject to change
-                        'seller_phone_number': str(self.mainwindow.bplr_seller_phone_number_lineedit.text()),
-                        'seller_email': str(self.mainwindow.bplr_seller_email_lineedit.text())
-                        }
-        banks = [self.mainwindow.bplr_bank1_checkbox, self.mainwindow.bplr_bank2_checkbox,
-                 self.mainwindow.bplr_bank3_checkbox,
-                 self.mainwindow.bplr_bank4_checkbox]
-        banks_ids = self.mainwindow.bank_ids
+        try:
+            self.payload = {'postal_code': str(self.mainwindow.bplr_postcode_lineedit.text()),
+                            'house_number': str(self.mainwindow.bplr_housenumber_lineedit.text()),
+                            'price': int(self.mainwindow.bplr_house_price_lineedit.text()),
+                            'amount_wanted': int(self.mainwindow.bplr_amount_wanted_lineedit.text()),
+                            'description': unicode(self.mainwindow.bplr_description_textedit.toPlainText()),
+                            'seller_phone_number': str(self.mainwindow.bplr_seller_phone_number_lineedit.text()),
+                            'seller_email': str(self.mainwindow.bplr_seller_email_lineedit.text()),
+                            'house_link': str(self.mainwindow.bplr_house_link_lineedit.text())
+                            }
+            banks = [self.mainwindow.bplr_bank1_checkbox, self.mainwindow.bplr_bank2_checkbox,
+                     self.mainwindow.bplr_bank3_checkbox,
+                     self.mainwindow.bplr_bank4_checkbox]
+            banks_ids = self.mainwindow.bank_ids
 
-        checked_banks = []
-        pointer = 0
-        #which banks were chosen
-        for obj in banks:
-            if obj.checkState():
-                checked_banks.append(banks_ids[pointer])
-            pointer += 1
+            checked_banks = []
+            pointer = 0
 
-        self.payload['banks'] = checked_banks
+            # Check which banks were chosen
+            for obj in banks:
+                if obj.checkState():
+                    checked_banks.append(banks_ids[pointer])
+                pointer += 1
 
-        #check the chosen mortgage type
-        self.payload['mortgage_type'] = 1
-        if self.mainwindow.bplr_linear_radiobutton.isChecked():
-            self.payload['mortgage_type'] = 0
+            self.payload['banks'] = checked_banks
 
-        if self.mainwindow.api.create_loan_request(self.mainwindow.app.user, self.payload):
-            QMessageBox.about(self.mainwindow, "Loan request created", 'Your loan request has been sent!')
-        else:
-            QMessageBox.about(self.mainwindow, "Loan request failed", 'You can only have a single loan request')
+            # Check the chosen mortgage type
+            self.payload['mortgage_type'] = 1
+            if self.mainwindow.bplr_linear_radiobutton.isChecked():
+                self.payload['mortgage_type'] = 0
+
+            if self.mainwindow.api.create_loan_request(self.mainwindow.app.user, self.payload):
+                QMessageBox.about(self.mainwindow, "Loan request created", 'Your loan request has been sent.')
+            else:
+                QMessageBox.about(self.mainwindow, "Loan request failed", 'You can only have a single loan request.')
+        except ValueError:
+            QMessageBox.about(self.mainwindow, "Loan request failed", 'You didn\'t enter the required information.')
