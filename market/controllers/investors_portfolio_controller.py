c1df83fcd580aa4c74f146f07ee8bc0ae811af94
Katia <katia.asmoredjo@gmail.com>
2016-12-16 16:25:51 +0100

    Implemented loading the table in investors' portfolio

--- market/controllers/investors_portfolio_controller.py
+++ market/controllers/investors_portfolio_controller.py
@@ -1,17 +1,45 @@
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
+import uuid
+from PyQt5 import QtWidgets
 
-from market.views.main_view import Ui_MainWindow
-from marketGUI.market_app import MarketApplication
-from market.api.api import MarketAPI
+from market.api.api import STATUS
 
 
 class InvestorsPortfolioController:
     def __init__(self, mainwindow):
-        # self.mainwindow = Ui_MainWindow  # Comment before running
-        self.mainwindow = mainwindow  # Uncomment before running
-
+        self.mainwindow = mainwindow
 
     def setup_view(self):
-        pass
+        # Clear table
+        investments_table = self.mainwindow.ip_investments_table
+        investments_table.clear()
+
+        # Getting the investments from the investor
+        investments = self.mainwindow.api.load_investments(self.mainwindow.app.user)
+
+        # If the list is empty, do nothing. Otherwise fill table
+        if investments:
+            # Fill the investments table
+            for [investment, house, campaign] in investments:
+                # Property Address, Campaign Status, Investment Status, Amount Invested, Interest, Duration
+                address = house.postal_code + ', ' + house.house_number
+
+                campaign_status = ''
+                if campaign.completed:
+                    campaign_status = 'Completed'
+                else:
+                    campaign_status = 'Running'
+
+                investment_status = ''
+                if investment.status == STATUS.ACCEPTED:
+                    investment_status = 'Accepted'
+                else:
+                    investment_status = 'Pending'
+
+                row_count = investments_table.rowCount()
+                investments_table.insertRow(row_count)
+                investments_table.setItem(row_count, 0, QtWidgets.QTableWidgetItem(address))
+                investments_table.setItem(row_count, 1, QtWidgets.QTableWidgetItem(campaign_status))
+                investments_table.setItem(row_count, 2, QtWidgets.QTableWidgetItem(investment_status))
+                investments_table.setItem(row_count, 3, QtWidgets.QTableWidgetItem(str(investment.amount)))
+                investments_table.setItem(row_count, 4, QtWidgets.QTableWidgetItem(str(investment.interest_rate)))
+                investments_table.setItem(row_count, 5, QtWidgets.QTableWidgetItem(str(investment.duration)))
