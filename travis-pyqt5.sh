3bea72288666a68052a2c38f03e6da44c434f4a8
seuushi <seumanto@gmail.com>
2016-12-16 11:45:16 +0100

    Added sip and pyqt5 dependency

---
+++ travis-pyqt5.sh
@@ -0,0 +1,6 @@
+wget http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=https%3A%2F%2Fwww.riverbankcomputing.com%2Fsoftware%2Fpyqt%2Fdownload5&ts=1481884926&use_mirror=kent
+tar xvfz PyQt5_gpl-5.7.tar.gz
+cd PyQt5_gpl-5.7
+./configure
+make
+make install
