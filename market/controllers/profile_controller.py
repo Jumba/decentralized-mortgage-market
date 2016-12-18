0faa44e4916f0f14d9b48e623f0373fcdbd084a3
Katia <katia.asmoredjo@gmail.com>
2016-12-18 18:39:40 +0100

    Added current address to the profile controller

--- market/controllers/profile_controller.py
+++ market/controllers/profile_controller.py
@@ -5,7 +5,6 @@ class ProfileController:
     def __init__(self, mainwindow):
         self.mainwindow = mainwindow
         self.current_profile = None
-        self.payload = {}
 
         # Add listener to the save profile button
         self.mainwindow.profile_save_pushbutton.clicked.connect(self.save_form)
@@ -25,6 +24,8 @@ class ProfileController:
                     self.mainwindow.profile_borrower_radiobutton.setChecked(True)
                     self.mainwindow.profile_postcode_lineedit.setText(self.current_profile.current_postal_code)
                     self.mainwindow.profile_housenumber_lineedit.setText(self.current_profile.current_house_number)
+                    self.mainwindow.profile_address_lineedit.setText(self.current_profile.current_address)
+
                 else:
                     self.mainwindow.profile_investor_radiobutton.setChecked(True)
             else:
@@ -32,23 +33,24 @@ class ProfileController:
 
     def save_form(self):
         # Get the data from the forms
-        self.payload = {'role': 2, 'first_name': unicode(self.mainwindow.profile_firstname_lineedit.text()),
+        payload = {'role': 2, 'first_name': unicode(self.mainwindow.profile_firstname_lineedit.text()),
                         'last_name': unicode(self.mainwindow.profile_lastname_lineedit.text()),
                         'email': str(self.mainwindow.profile_email_lineedit.text()),
                         'iban': str(self.mainwindow.profile_iban_lineedit.text()),
                         'phonenumber': str(self.mainwindow.profile_phonenumber_lineedit.text())}
 
         if self.mainwindow.profile_borrower_radiobutton.isChecked():
-            self.payload['role'] = 1
-            self.payload['current_postalcode'] = str(self.mainwindow.profile_postcode_lineedit.text())
-            self.payload['current_housenumber'] = str(self.mainwindow.profile_housenumber_lineedit.text())
+            payload['role'] = 1
+            payload['current_postalcode'] = str(self.mainwindow.profile_postcode_lineedit.text())
+            payload['current_housenumber'] = str(self.mainwindow.profile_housenumber_lineedit.text())
+            payload['current_address'] = str(self.mainwindow.profile_address_lineedit.text())
             # TODO Add missing 'documents_list': self.documentsTable
-            self.payload['documents_list'] = []
+            payload['documents_list'] = []
 
         # Check if the user can switch roles
         if self.check_role_switch():
             # Show a popup window when the profile has been saved
-            if self.mainwindow.api.create_profile(self.mainwindow.app.user, self.payload):
+            if self.mainwindow.api.create_profile(self.mainwindow.app.user, payload):
                 QMessageBox.about(self.mainwindow, "Profile saved", 'Your profile has been saved.')
             self.mainwindow.app.user.update(self.mainwindow.api.db)
             self.update_navigation_bar()
