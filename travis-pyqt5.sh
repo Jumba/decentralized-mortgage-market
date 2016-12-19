618b8ed169f731bda08fd7472ae993bf2b5a47c4
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-12-19 12:07:56 +0100

    Fixing pyqt5 install

--- travis-pyqt5.sh
+++ travis-pyqt5.sh
@@ -1,6 +1,6 @@
-wget http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=https%3A%2F%2Fwww.riverbankcomputing.com%2Fsoftware%2Fpyqt%2Fdownload5&ts=1481884926&use_mirror=kent
-tar xvfz PyQt5_gpl-5.7.tar.gz
+wget -O PyQt5_gpl-5.7.tar.gz http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=https%3A%2F%2Fwww.riverbankcomputing.com%2Fsoftware%2Fpyqt%2Fdownload5&ts=1481884926&use_mirror=kent
+tar zxvf PyQt5_gpl-5.7.tar.gz
 cd PyQt5_gpl-5.7
-./configure
+python configure.py
 make
 sudo make install
