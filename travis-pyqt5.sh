a882267453843e13702eda7b37c0f42bf3ab89b0
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-12-19 12:24:24 +0100

    Another one

--- travis-pyqt5.sh
+++ travis-pyqt5.sh
@@ -1,1 +1,6 @@
-sudo apt-get install python-pyqt5
+wget -O PyQt5_gpl-5.7.tar.gz http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=https%3A%2F%2Fwww.riverbankcomputing.com%2Fsoftware%2Fpyqt%2Fdownload5&ts=1481884926&use_mirror=kent
+tar zxvf PyQt5_gpl-5.7.tar.gz
+cd PyQt5_gpl-5.7
+python configure.py --qmake /usr/lib/x86_64-linux-gnu/qt5/bin/qmake  --sip-incdir ../sip-4.18.1/siplib
+make
+sudo make install
