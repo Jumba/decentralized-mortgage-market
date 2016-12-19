71f51ea9ce8395c7b0fbb2a82503732385f2cdc0
Katia <katia.asmoredjo@gmail.com>
2016-12-19 12:00:35 +0100

    Trying to fix travis

--- travis-sip.sh
+++ travis-sip.sh
@@ -1,6 +1,5 @@
-wget https://sourceforge.net/projects/pyqt/files/sip/sip-4.18.1/sip-4.18.1.tar.gz/download
-tar xvfz sip-4.18.1.tar.gz
-cd sip-4.18.1
-./configure
-make
-sudo make install
+sudo mkdir -p /builds && sudo chmod a+rw /builds
+sudo mkdir -p /downloads && sudo chmod a+rw /downloads
+curl -L -o /downloads/sip.tar.gz https://sourceforge.net/projects/pyqt/files/sip/sip-4.18.1/sip-4.18.1.tar.gz
+tar xzf /downloads/sip.tar.gz -C /builds --keep-newer-files
+cd /builds/sip-4.18.1 && python configure.py && sudo make && sudo make install
