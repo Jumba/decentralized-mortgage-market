186ef90cef8bef7dc0061d1619f732277d3f52e9
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-12-19 12:02:09 +0100

    Fixed sip install

--- travis-sip.sh
+++ travis-sip.sh
@@ -1,6 +1,6 @@
-wget https://sourceforge.net/projects/pyqt/files/sip/sip-4.18.1/sip-4.18.1.tar.gz/download
-tar xvfz sip-4.18.1.tar.gz
+wget -O sip-4.18.1.tar.gz https://sourceforge.net/projects/pyqt/files/sip/sip-4.18.1/sip-4.18.1.tar.gz/download
+tar zvxf sip-4.18.1.tar.gz
 cd sip-4.18.1
-./configure
+python configure.py
 make
 sudo make install
