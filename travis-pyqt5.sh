bae6ae8dddc01248eaaba4c08c3e176f5bd93fae
Katia <katia.asmoredjo@gmail.com>
2016-12-19 15:04:56 +0100

    MAJOR KEY

--- travis-pyqt5.sh
+++ travis-pyqt5.sh
@@ -1,10 +1,10 @@
-# sudo add-apt-repository ppa:beineri/opt-qt551 -y
-# sudo apt-get -qq update
-# sudo apt-get -qq install qtdeclarative5-dev qt5-default
+sudo add-apt-repository ppa:beineri/opt-qt551 -y
+sudo apt-get -qq update
+sudo apt-get -qq install qtdeclarative5-dev qt5-default
 
-# wget -O PyQt5_gpl-5.7.tar.gz "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=&ts=1482146895&use_mirror=vorboss"
-# tar zxf PyQt5_gpl-5.7.tar.gz
-# cd PyQt5_gpl-5.7
-# python configure.py --confirm-license --qmake /usr/lib/x86_64-linux-gnu/qt5/bin/qmake  --sip-incdir ../sip-4.18.1/siplib
-# make
-# sudo make install
+wget -O PyQt5_gpl-5.7.tar.gz "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=&ts=1482146895&use_mirror=vorboss"
+tar zxf PyQt5_gpl-5.7.tar.gz
+cd PyQt5_gpl-5.7
+python configure.py --confirm-license --qmake /usr/lib/x86_64-linux-gnu/qt5/bin/qmake  --sip-incdir ../sip-4.18.1/siplib
+make
+sudo make install
