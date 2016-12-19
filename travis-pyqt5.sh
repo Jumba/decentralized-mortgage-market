a5c0387081c48d91f8610e8508f465926765f6ae
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-12-19 12:35:49 +0100

    ayyyyy

--- travis-pyqt5.sh
+++ travis-pyqt5.sh
@@ -1,5 +1,5 @@
 wget -O PyQt5_gpl-5.7.tar.gz "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz?r=&ts=1482146895&use_mirror=vorboss"
-tar zxvf PyQt5_gpl-5.7.tar.gz
+tar zxf PyQt5_gpl-5.7.tar.gz
 cd PyQt5_gpl-5.7
 python configure.py --qmake /usr/lib/x86_64-linux-gnu/qt5/bin/qmake  --sip-incdir ../sip-4.18.1/siplib
 make
