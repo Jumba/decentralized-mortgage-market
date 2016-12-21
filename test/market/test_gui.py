ff8bf4ad7af128ca3ee9bc0ceec3d0829673c0bd
arthur <a.hovanesyan@gmail.com>
2016-12-21 14:42:27 +0100

    Added tests for profile

--- test/market/test_gui.py
+++ test/market/test_gui.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import os
 import sys
 import unittest
+from uuid import UUID
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtTest import QTest
@@ -32,35 +33,112 @@ class GUITestSuite(unittest.TestCase):
 
     def test_profile_empty(self):
         # Test that the controller calls a QMessageBox with unique input when the form has not been filled in
-        self.window.profile_controller.msg.about = MagicMock()
+        self.window.msg.about = MagicMock()
         QTest.mouseClick(self.window.profile_save_pushbutton, Qt.LeftButton)
-        self.window.profile_controller.msg.about.assert_called_with(self.window, 'Profile error', 'You didn\'t enter all of the required information.')
+        self.window.msg.about.assert_called_with(self.window, 'Profile error', 'You didn\'t enter all of the required information.')
+
+    def test_profile_create_profile(self):
+        # Testing loading of the current borrower's profile
+        payload = {'role': 1, 'first_name': u'Bob', 'last_name': u'Saget', 'email': 'example@example.com',
+                        'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719234',
+                        'current_postalcode': '2162CD', 'current_housenumber': '22', 'current_address': 'straat',
+                        'documents_list': []}
+        self.assertEqual(None, self.window.profile_controller.current_profile)
+
+        # Create profile so when we can populate the form easier
+        user, _, _ = self.app.api.create_user()
+        profile = self.app.api.create_profile(user, payload)
+        self.assertNotEqual(None, profile)
+
+        # Fill in the form and press the 'save'-button
+        self.window.profile_controller.update_form(profile)
+        self.window.msg.about = MagicMock()
+        QTest.mouseClick(self.window.profile_save_pushbutton, Qt.LeftButton)
+        self.window.msg.about.assert_called_with(self.window, 'Profile saved', 'Your profile has been saved.')
 
     def test_profile_load_current_borrower(self):
         # Testing loading of the current borrower's profile
-        self.payload = {'role': 1, 'first_name': u'Bob', 'last_name': u'Saget', 'email': 'example@example.com',
+        payload = {'role': 1, 'first_name': u'Bob', 'last_name': u'Saget', 'email': 'example@example.com',
                         'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719234',
                         'current_postalcode': '2162CD', 'current_housenumber': '22', 'current_address': 'straat',
                         'documents_list': []}
-        self.payload_investor = {'role': 2, 'first_name': u'Ruby', 'last_name': u'Cue', 'email': 'example1@example.com',
-                                 'iban': 'NL53 INGB 04097 30393', 'phonenumber': '+3170253719290'}
-        pass
+        self.assertEqual(None, self.window.profile_controller.current_profile)
+        profile = self.app.api.create_profile(self.app.user, payload)
+        self.assertNotEqual(None, profile)
+        self.window.profile_controller.setup_view()
+        # Check if the input fields are the same as the payload
+        self.assertEqual(self.window.profile_firstname_lineedit.text(), 'Bob')
+        self.assertEqual(self.window.profile_lastname_lineedit.text(), 'Saget')
+        self.assertEqual(self.window.profile_email_lineedit.text(), 'example@example.com')
+        self.assertEqual(self.window.profile_iban_lineedit.text(), 'NL53 INGBB 04027 30393')
+        self.assertEqual(self.window.profile_phonenumber_lineedit.text(), '+3170253719234')
+        self.assertEqual(self.window.profile_postcode_lineedit.text(), '2162CD')
+        self.assertEqual(self.window.profile_housenumber_lineedit.text(), '22')
+        self.assertEqual(self.window.profile_address_lineedit.text(), 'straat')
 
     def test_profile_load_current_investor(self):
-        # Testing loading of the current investor's profile
-        self.payload = {'role': 1, 'first_name': u'Bob', 'last_name': u'Saget', 'email': 'example@example.com',
-                        'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719234',
-                        'current_postalcode': '2162CD', 'current_housenumber': '22', 'current_address': 'straat',
-                        'documents_list': []}
-        self.payload_investor = {'role': 2, 'first_name': u'Ruby', 'last_name': u'Cue', 'email': 'example1@example.com',
-                                 'iban': 'NL53 INGB 04097 30393', 'phonenumber': '+3170253719290'}
-        pass
+        payload_investor = {'role': 2, 'first_name': u'Ruby', 'last_name': u'Cue', 'email': 'example1@example.com',
+                                 'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719290'}
+        profile = self.app.api.create_profile(self.app.user, payload_investor)
+        self.assertNotEqual(None, profile)
+        self.window.profile_controller.setup_view()
+        # Check if the input fields are the same as the payload
+        self.assertEqual(self.window.profile_firstname_lineedit.text(), 'Ruby')
+        self.assertEqual(self.window.profile_lastname_lineedit.text(), 'Cue')
+        self.assertEqual(self.window.profile_email_lineedit.text(), 'example1@example.com')
+        self.assertEqual(self.window.profile_iban_lineedit.text(), 'NL53 INGBB 04027 30393')
+        self.assertEqual(self.window.profile_phonenumber_lineedit.text(), '+3170253719290')
 
     def test_profile_switch_role_valid(self):
-        pass
+        # A user is only allowed to switch roles when he has no active loan requests or campaigns
+        payload = {'role': 1, 'first_name': u'Bob', 'last_name': u'Saget', 'email': 'example@example.com',
+                   'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719234',
+                   'current_postalcode': '2162CD', 'current_housenumber': '22', 'current_address': 'straat',
+                   'documents_list': []}
+        payload_investor = {'role': 2, 'first_name': u'Ruby', 'last_name': u'Cue', 'email': 'example1@example.com',
+                            'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719290'}
+        profile = self.app.api.create_profile(self.app.user, payload)
+        self.assertNotEqual(None, profile)
+        # self.window.profile_controller.setup_view()
+
+        # Try to switch from borrower to investor
+        user, _, _ = self.app.api.create_user()
+        new_profile = self.app.api.create_profile(user, payload_investor)
+        self.app.user.role_id = 2
+        self.window.profile_controller.update_form(new_profile)
+
+        self.window.msg.about = MagicMock()
+        QTest.mouseClick(self.window.profile_save_pushbutton, Qt.LeftButton)
+        self.window.msg.about.assert_called_with(self.window, 'Profile saved', 'Your profile has been saved.')
 
     def test_profile_switch_role_invalid(self):
-        pass
+        # A user is only allowed to switch roles when he has no active loan requests or campaigns
+        payload = {'role': 1, 'first_name': u'Bob', 'last_name': u'Saget', 'email': 'example@example.com',
+                   'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719234',
+                   'current_postalcode': '2162CD', 'current_housenumber': '22', 'current_address': 'straat',
+                   'documents_list': []}
+        payload_investor = {'role': 2, 'first_name': u'Ruby', 'last_name': u'Cue', 'email': 'example1@example.com',
+                            'iban': 'NL53 INGBB 04027 30393', 'phonenumber': '+3170253719290'}
+        payload_loan_request = {'house_id': UUID('b97dfa1c-e125-4ded-9b1a-5066462c520c') , 'mortgage_type': 1, 'banks': [],
+                                     'description': unicode('I want to buy a house'), 'amount_wanted': 123456, 'postal_code' : '1111AA', 'house_number' : '11', 'address' : 'straat', 'price' : 123456,
+                                     'house_link': 'http://www.myhouseee.com/', 'seller_phone_number': '0612345678',
+                                     'seller_email': 'seller1@gmail.com'}
+        profile = self.app.api.create_profile(self.app.user, payload)
+        self.assertNotEqual(None, profile)
+        # self.window.profile_controller.setup_view()
+
+        # Place a loan request
+        self.app.api.create_loan_request(self.app.user, payload_loan_request)
+        # Try to switch from borrower to investor
+        user, _, _ = self.app.api.create_user()
+        new_profile = self.app.api.create_profile(user, payload_investor)
+        self.app.user.role_id = 2
+        self.window.profile_controller.update_form(new_profile)
+
+        self.window.msg.about = MagicMock()
+        QTest.mouseClick(self.window.profile_save_pushbutton, Qt.LeftButton)
+        self.window.msg.about.assert_called_with(self.window, 'Role switch failed',
+                                                 'It is not possible to change your role at this time')
 
     def test_investors_portfolio_table_empty(self):
         pass
