a87ed6af806f53ba86133cda3c2ce35e53af0b2a
arthur <a.hovanesyan@gmail.com>
2017-01-03 14:09:45 +0100

    The tftp listener now runs on a different thread

--- run_tftp_client.py
+++ run_tftp_client.py
@@ -1,5 +1,5 @@
 import tftpy
 import socket
 
-client = tftpy.TftpClient(socket.gethostbyname(socket.gethostname()), 69)
+client = tftpy.TftpClient(socket.gethostbyname(socket.gethostname()), 6969)
 client.download('server.txt', 'downloaded.txt')
--- run_tftp_server.py
+++ run_tftp_server.py
@@ -1,5 +1,24 @@
 import tftpy
 import os
+import threading
 
-server = tftpy.TftpServer(os.getcwd())
-server.listen()
+
+class TftpServer():
+    def __init__(self):
+        self.server = tftpy.TftpServer(os.getcwd())
+        self.server_thread = threading.Thread(target=self.server_start)
+
+    def stop(self):
+        if self.server.is_running:
+            self.server.stop()
+
+    def start(self):
+        self.server_thread.start()
+
+    def server_start(self):
+        self.server.listen(listenport=6969)
+
+
+if __name__ == '__main__':
+    ts = TftpServer()
+    ts.start()
