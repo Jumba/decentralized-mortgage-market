825870f1621e91fb6a9868d4f37e66a927f632a6
arthur <a.hovanesyan@gmail.com>
2017-01-05 17:27:56 +0100

    Client now runs on a thread. Logs are written to file.

--- run_tftp_client.py
+++ run_tftp_client.py
@@ -2,12 +2,23 @@ import socket
 import os
 import glob
 import ntpath
+import tftpy
 from tftpy import TftpClient
 from threading import Thread
+import time
+import logging
+
+RESOURCES_PATH = '/resources/received/'
+
 
 class Client:
     def __init__(self):
         self.resources = None
+        tftpy.setLogLevel('INFO')
+        fh = logging.FileHandler(os.getcwd()+'/logging/log_client_'+time.strftime('%d-%m-%Y_%H:%M:%S'))
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        tftpy.log.addHandler(fh)
         self.client = TftpClient(socket.gethostbyname(socket.gethostname()), 50000)
         self.sent_files = []
         self.files = []
@@ -16,21 +27,36 @@ class Client:
 
     def upload(self, local_file_name, remote_file_name=None):
         if not remote_file_name:
-            remote_file_name = '/resources/received/' + ntpath.basename(local_file_name)
+            remote_file_name = RESOURCES_PATH + ntpath.basename(local_file_name)
         try:
             self.client.upload(remote_file_name, local_file_name)
+            self.sent_files.append(local_file_name)
         except:
             print 'Uploading of ', remote_file_name, ' failed.'
+            self.failed_to_send.append(remote_file_name)
             raise
-            # TODO add the file to queue of failed uploads
 
     def download(self, remote_file_name, local_file_name=None):
         if not local_file_name:
-            local_file_name = '/resources/received/' + ntpath.basename(remote_file_name)
+            local_file_name = RESOURCES_PATH + ntpath.basename(remote_file_name)
         try:
             self.client.download(local_file_name, remote_file_name)
         except:
             raise
+
+    # def upload(self, local_file_name, remote_file_name):
+    #     if self.thread.is_alive():
+    #         print 'The thread is busy.'
+    #     else:
+    #         self.thread = Thread(target=self.upload_to_server(local_file_name, remote_file_name))
+    #         self.thread.start()
+    #
+    # def download(self, remote_file_name, local_file_name):
+    #     if self.thread.is_alive():
+    #         print 'The thread is busy.'
+    #     else:
+    #         self.thread = Thread(target=self.download_from_server(remote_file_name, local_file_name))
+    #         self.thread.start()
 
     def upload_folder(self, path=os.getcwd()+'/resources'):
         self.files = glob.glob(path+'/*.pdf')
@@ -41,6 +67,8 @@ class Client:
 if __name__ == '__main__':
     tc = Client()
     # tc.upload('server.pdf', 'downloaded.pdf')
-    tc.upload_folder()
+    # tc.thread.target = tc.upload_folder
+    thread = Thread(target=tc.upload_folder())
+    thread.start()
 
 
--- run_tftp_server.py
+++ run_tftp_server.py
@@ -1,12 +1,26 @@
 import tftpy
 import os
 import threading
+import time
+import logging
 
 
 class Server:
     def __init__(self):
         self.server = tftpy.TftpServer(os.getcwd())
-        self.thread = threading.Thread(target=self.server_start)
+        tftpy.setLogLevel('INFO')
+        fh = logging.FileHandler(os.getcwd()+'/logging/log_server_'+time.strftime('%d-%m-%Y_%H:%M:%S'))
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        tftpy.log.addHandler(fh)
+        try:
+            self.thread = threading.Thread(target=self.server_start)
+        except:
+            tftpy.log.error('The socket was not properly closed after the last run. '
+                            'Please kill the process or try again in a couple of minutes.'
+                            'Process id can be found by using the \'lsof -i UDP\' command '
+                            'to find the right PID then followed by \'kill PID\' ')
+            raise
 
     def stop(self):
         if self.server.is_running:
@@ -18,7 +32,15 @@ class Server:
     def server_start(self):
         self.server.listen(listenport=50000)
 
+    @staticmethod
+    def set_logging(level, filename=None):
+        if not file:
+            tftpy.log.FileHandler(os.getcwd()+'/logging/'+filename)
+        tftpy.setLogLevel(level)
+
 
 if __name__ == '__main__':
     ts = Server()
     ts.start()
+    time.sleep(3)
+    ts.stop()
