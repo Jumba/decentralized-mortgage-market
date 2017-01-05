85e38f8b42be5bcbd74579b9c70500ddc70cc72a
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2017-01-05 18:34:29 +0100

    Added argparse to organize everything a bit better

--- market.py
+++ market.py
@@ -1,27 +1,48 @@
 import sys
-from marketGUI.market_app import MarketApplication
+import argparse
+
+from marketGUI.market_app import MarketApplication, MarketApplicationING, MarketApplicationRABO, MarketApplicationMONEYOU, \
+    MarketApplicationBank, MarketApplicationABN
 from scenarios.apps import MarketAppSceneBank, MarketAppSceneBankING, MarketAppSceneBankRABO, MarketAppSceneBankMONEYOU, \
     MarketAppSceneBorrower, MarketAppSceneInvestor
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--headless", help="Run the market in headless mode", action="store_true")
+    parser.add_argument("--bank", help="Run the market as a bank.", type=str, choices=['abn', 'ing', 'rabo', 'moneyou'],)
+    parser.add_argument("--scenario", help="Select a scenario to enable", type=str, choices=['bank', 'borrower', 'investor'],)
+
+    args = parser.parse_args()
+
+    if not args.bank and not args.scenario:
         app = MarketApplication(sys.argv)
-    else:
-        bank = sys.argv[1]
-        if bank == "abn":
+    elif args.scenario == 'borrower':
+        app = MarketAppSceneBorrower(sys.argv)
+    elif args.scenario == 'investor':
+        app = MarketAppSceneInvestor(sys.argv)
+    elif args.bank == "abn":
+        if args.scenario == "bank":
             app = MarketAppSceneBank(sys.argv)
-        elif bank == "ing":
-            app = MarketAppSceneBankING(sys.argv)
-        elif bank == "rabo":
-            app = MarketAppSceneBankRABO(sys.argv)
-        elif bank == "moneyou":
-            app = MarketAppSceneBankMONEYOU(sys.argv)
-        elif bank == "borrower":
-            app = MarketAppSceneBorrower(sys.argv)
-        elif bank == "investor":
-            app = MarketAppSceneInvestor(sys.argv)
         else:
-            raise SystemExit("Unknown bank")
+            app = MarketApplicationABN(sys.argv)
+    elif args.bank == "ing":
+        if args.scenario == "bank":
+            app = MarketAppSceneBankING(sys.argv)
+        else:
+            app = MarketApplicationING(sys.argv)
+    elif args.bank == "rabo":
+        if args.scenario == "bank":
+            app = MarketAppSceneBankRABO(sys.argv)
+        else:
+            app = MarketApplicationRABO(sys.argv)
+    elif args.bank == "moneyou":
+        if args.scenario == "bank":
+            app = MarketAppSceneBankMONEYOU(sys.argv)
+        else:
+            app = MarketApplicationMONEYOU(sys.argv)
+    else:
+        raise SystemExit("Unknown bank")
 
     headless = False
     if len(sys.argv) == 3:
