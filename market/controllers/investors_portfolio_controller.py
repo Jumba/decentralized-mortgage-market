e0bab7585cd5cc4c9d9d73bf67f9fc5d0999524c
Katia <katia.asmoredjo@gmail.com>
2017-01-18 10:51:43 +0100

    Refactored the investor's portfolio controller to reduce unit size

--- market/controllers/investors_portfolio_controller.py
+++ market/controllers/investors_portfolio_controller.py
@@ -14,7 +14,7 @@ class InvestorsPortfolioController:
 
     def setup_view(self):
         """
-        Update the table of the view with up-to-date data.
+        Sets up the view.
         """
         # Clear table
         self.investments_table.setRowCount(0)
@@ -24,32 +24,37 @@ class InvestorsPortfolioController:
 
         # If the list is empty, do nothing. Otherwise fill table
         if investments:
-            # Fill the investments table
-            for investment, house, campaign, profile in investments:
-                # Property Address, Campaign Status, Investment Status, Amount Invested, Interest, Duration
-                address = house.address + ' ' + house.house_number + ', ' + house.postal_code
-                name = ' '
-                iban = ' '
+            self.fill_table(investments)
 
-                if campaign.completed:
-                    campaign_status = 'Completed'
-                else:
-                    campaign_status = 'Running'
+    def fill_table(self, investments):
+        """
+        Fills the table with investments.
+        """
+        for investment, house, campaign, profile in investments:
+            # Property Address, Campaign Status, Investment Status, Amount Invested, Interest, Duration
+            address = house.address + ' ' + house.house_number + ', ' + house.postal_code
+            name = ' '
+            iban = ' '
 
-                if investment.status == STATUS.ACCEPTED:
-                    investment_status = 'Accepted'
-                    name = profile.first_name + ' ' + profile.last_name
-                    iban = profile.iban
-                else:
-                    investment_status = 'Pending'
+            if campaign.completed:
+                campaign_status = 'Completed'
+            else:
+                campaign_status = 'Running'
 
-                row_count = self.investments_table.rowCount()
-                self.investments_table.insertRow(row_count)
-                self.investments_table.setItem(row_count, 0, QtWidgets.QTableWidgetItem(address))
-                self.investments_table.setItem(row_count, 1, QtWidgets.QTableWidgetItem(campaign_status))
-                self.investments_table.setItem(row_count, 2, QtWidgets.QTableWidgetItem(investment_status))
-                self.investments_table.setItem(row_count, 3, QtWidgets.QTableWidgetItem(str(investment.amount)))
-                self.investments_table.setItem(row_count, 4, QtWidgets.QTableWidgetItem(str(investment.interest_rate)))
-                self.investments_table.setItem(row_count, 5, QtWidgets.QTableWidgetItem(str(investment.duration)))
-                self.investments_table.setItem(row_count, 6, QtWidgets.QTableWidgetItem(name))
-                self.investments_table.setItem(row_count, 7, QtWidgets.QTableWidgetItem(iban))
+            if investment.status == STATUS.ACCEPTED:
+                investment_status = 'Accepted'
+                name = profile.first_name + ' ' + profile.last_name
+                iban = profile.iban
+            else:
+                investment_status = 'Pending'
+
+            row_count = self.investments_table.rowCount()
+            self.investments_table.insertRow(row_count)
+            self.investments_table.setItem(row_count, 0, QtWidgets.QTableWidgetItem(address))
+            self.investments_table.setItem(row_count, 1, QtWidgets.QTableWidgetItem(campaign_status))
+            self.investments_table.setItem(row_count, 2, QtWidgets.QTableWidgetItem(investment_status))
+            self.investments_table.setItem(row_count, 3, QtWidgets.QTableWidgetItem(str(investment.amount)))
+            self.investments_table.setItem(row_count, 4, QtWidgets.QTableWidgetItem(str(investment.interest_rate)))
+            self.investments_table.setItem(row_count, 5, QtWidgets.QTableWidgetItem(str(investment.duration)))
+            self.investments_table.setItem(row_count, 6, QtWidgets.QTableWidgetItem(name))
+            self.investments_table.setItem(row_count, 7, QtWidgets.QTableWidgetItem(iban))
